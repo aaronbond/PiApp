f37da4979ec857fbed513aac05687e33af8275c8
Aaron Bond <aaronbond@outlook.com>
2018-11-12 10:03:51 +1100

    Add helper methods

---
+++ app/src/main/java/com/example/aaronbond/piapp/util/Extensions.kt
@@ -0,0 +1,22 @@
+package com.example.aaronbond.piapp.util
+
+import java.math.BigDecimal
+
+inline fun <T> Iterable<T>.sumByBigDecimal(selector: (T) -> BigDecimal): BigDecimal {
+    var sum = 0.0.toBigDecimal()
+
+    for (element in this) {
+        sum += selector(element)
+    }
+
+    return sum
+}
+
+fun Int.isEven() : Boolean {
+    return this % 2 == 0
+}
+
+fun Long.isEven() : Boolean {
+    return this % 2 == 0L
+}
+
